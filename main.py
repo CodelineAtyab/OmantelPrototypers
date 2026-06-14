73fd9561ec304f327533d193931852e4600aaed6
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-06-14 12:19:08 +0400

    Refactored the function to match it with the correct behavior

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def add_feedback_for_user(user_nick_name, feedback_msg, feedback_bucket):
 
 def get_all_feedbacks_for_user(user_nick_name, feedback_bucket):
     user_specific_list = feedback_bucket[user_nick_name]
-    for feedback in user_specific_list:
-       print(feedback)
+    return user_specific_list
 
 
 # Store dummy feedbacks for all 3 end users
@@ -27,5 +26,5 @@ add_feedback_for_user("MR.Z", "Something is missing!", feedback_store)
 
 
 # Output (Provide Arguments to the function)
-get_all_feedbacks_for_user("MR.Y", feedback_store)
+print(get_all_feedbacks_for_user("MR.X", feedback_store))
 print("Exiting Application!")
